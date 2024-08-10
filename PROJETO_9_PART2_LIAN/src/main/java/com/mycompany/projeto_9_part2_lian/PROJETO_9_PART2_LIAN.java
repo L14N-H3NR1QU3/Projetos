2b319119858d42e4328662d6aee0ab69047176cd
L14N-H3NR1QU3 <lian.h.silva@aluno.senai.br>
2024-08-10 01:46:07 -0300

    Atualizando 9!

---
+++ PROJETO_9_PART2_LIAN/src/main/java/com/mycompany/projeto_9_part2_lian/PROJETO_9_PART2_LIAN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_9_part2_lian;
+
+/**
+ *
+ * @author Ryzen
+ */
+public class PROJETO_9_PART2_LIAN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
