e2ec4e8880375d7879b4f513e844152b45576e71
L14N-H3NR1QU3 <lian.h.silva@aluno.senai.br>
2024-08-10 03:11:13 -0300

    Finalizado projeto 9

--- PROJETO_9_LIAN/src/main/java/com/mycompany/projeto_9_lian/PROJETO_9_LIAN.java
+++ PROJETO_9_LIAN/src/main/java/com/mycompany/projeto_9_lian/PROJETO_9_LIAN.java
@@ -8,24 +8,60 @@ package com.mycompany.projeto_9_lian;
  *
  * @author Ryzen
  */
+import java.util.Random;
 public class PROJETO_9_LIAN {
-
     public static void main(String[] args) {
-        int aux = 0;
-        for(int i = 0; i < 600; i++){
-            if(aux == 80){
-                aux = 0;
+        Random rand = new Random();
+        int carros = rand.nextInt(10);
+        int totalCarros1 = carros;
+        int totalCarros2 = 0;
+        int totalCarros3 = 0;
+        int novosCarros = 0;
+        int tempo = 0;
+        for(int i = 1; i <= 3; i++){
+            /*as 3 vias*/
+            for(int j = 0; j < 600; j++){
+                if(tempo == 80){
+                    /*tempo em que ocorre o ciclo do semaforo*/
+                    tempo = 0;
+                }
+                if(novosCarros == 15){
+                    /*tampo com que ocorre a entrada de carros*/
+                    novosCarros = 0;
+                    if(carros < 10){
+                        carros += rand.nextInt(10);
+                        if(i == 1){
+                            totalCarros1 += carros;
+                        }
+                        else if(i == 2){
+                            totalCarros2 += carros;
+                        }
+                        else{
+                            totalCarros3 += carros;
+                        }
+                    }
+                }
+                else if(tempo <= 30){
+                    System.out.println("Verde no sinal "+i);
+                    while(carros > 0){
+                        /*saida de carros*/
+                        System.out.println("Carro sai no sinal "+i);
+                        carros--;
+                        tempo++;
+                    }
+                }
+                else if((tempo > 30) && (tempo <= 35)){
+                    System.out.println("Amarelo no sinal "+i);
+                }
+                else{
+                    System.out.println("Vermelho no sinal "+i);
+                }
+                novosCarros++;
+                tempo++;
             }
-            else if(aux <= 30){
-                System.out.println("Verde");
-            }
-            else if((aux > 30) && (aux <= 35)){
-                System.out.println("Amarelo");
-            }
-            else{
-                System.out.println("Vermelho");
-            }
-            aux++;
         }
+        System.out.println("Sairam "+totalCarros1+" carros da primeira intersecao");
+        System.out.println("Sairam "+totalCarros2+" carros da segunda intersecao");
+        System.out.println("Sairam "+totalCarros3+" carros da terceira intersecao");
     }
 }
