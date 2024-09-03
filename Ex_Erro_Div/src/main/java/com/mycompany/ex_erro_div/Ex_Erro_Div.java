ea5e5af0568b058b1e150d5c0f612c64bd378765
L14N-H3NR1QU3 <lian.h.silva@aluno.senai.br>
2024-09-03 14:07:47 -0300

    PROJETO 13

--- Ex_Erro_Div/src/main/java/com/mycompany/ex_erro_div/Ex_Erro_Div.java
+++ Ex_Erro_Div/src/main/java/com/mycompany/ex_erro_div/Ex_Erro_Div.java
@@ -13,32 +13,37 @@ public class Ex_Erro_Div {
 
     public static void main(String[] args) {
         Scanner ler = new Scanner (System.in);
-        float vet[] = new float [4];
+        double vet[] = new double [4];
         for(int i = 0; i < 4; i++){
             System.out.print("Digite um numero: ");
             String res = ler.next();
             try{
-                float num = Integer.parseInt(res);
+                double num = Integer.parseInt(res);
                 vet[i] = num;
             }
             catch (NumberFormatException e2){
-                System.out.println("Erro: argumento invalido.");
+                System.out.println("Erro: argumento indisponivael.");
                 break;
             }
         }
-        try{
-            double res1 = vet[0]/vet[1];
-            System.out.println("Resultado 1: "+res1);
+        if(vet[3] != 0){
+            try{
+                double res1 = vet[0]/vet[1];
+                System.out.println("Resultado 1: "+res1);
+            }
+            catch (ArithmeticException e){
+                System.out.println("Erro: calculo indisponivael");
+            }
+            try{
+                double res2 = vet[2]/vet[3];
+                System.out.println("Resultado 2: "+res2);
+            }
+            catch (ArithmeticException e){
+                System.out.println("Erro: calculo indisponivael.");
+            }
         }
-        catch (ArithmeticException e){
-            System.out.println("Erro: calculo invalido");
-        }
-        try{
-            double res2 = vet[2]/vet[3];
-            System.out.println("Resultado 2: "+res2);
-        }
-        catch (ArithmeticException e){
-            System.out.println("Erro: calculo invalido.");
+        else{
+            System.out.println("Erro: calculo indisponivael.");
         }
         ler.close();
     }
