2f97d221e463d9c79447927dbfdbb290c01d4054
L14N-H3NR1QU3 <lian.h.silva@aluno.senai.br>
2024-09-06 14:37:55 -0300

    Atividade 14

--- PROJETO_14_LIAN/src/main/java/com/mycompany/projeto_14_lian/PROJETO_14_LIAN.java
+++ PROJETO_14_LIAN/src/main/java/com/mycompany/projeto_14_lian/PROJETO_14_LIAN.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.projeto_14_lian;
 
 import java.io.DataInputStream;
@@ -14,37 +13,36 @@ import java.io.IOException;
 public class PROJETO_14_LIAN {
 
     public static void main(String[] args) throws IOException {
-        int n = 2;
-        int num[][] = new int [n] [n];
+        int n = 3;
+        int num[][] = new int[n][n];
         int repNum = 0;
-        int iden = 0;
+        int auxNum = 0;
         DataInputStream dado;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.println("Digite um numero: ");
-                dado = new DataInputStream (System.in);
+                dado = new DataInputStream(System.in);
                 String res = dado.readLine();
-                try{
+                try {
                     num[i][j] = Integer.parseInt(res);
-                }
-                catch(NumberFormatException e){
+                } catch (NumberFormatException e) {
                     System.out.println("Erro: Argumento invalido.");
-                    i--;
+                    j--;
                 }
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                System.out.print("["+num[i][j]+"]");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("[" + num[i][j] + "]");
             }
             System.out.print("\n");
         }
         System.out.println("\n");
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                for(int k = 0; k < n; k++){
-                    for(int l = 0; l < n; l++){
-                        if(num[i][j] <= num[k][l]){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < n; l++) {
+                        if (num[i][j] <= num[k][l]) {
                             int aux = num[i][j];
                             num[i][j] = num[k][l];
                             num[k][l] = aux;
@@ -53,32 +51,52 @@ public class PROJETO_14_LIAN {
                 }
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                System.out.print("["+num[i][j]+"]");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("[" + num[i][j] + "]");
             }
         }
         System.out.println("\n");
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int cal = 0;
-            for(int j = 0; j < n; j++){
+            for (int j = 0; j < n; j++) {
                 cal += num[i][j];
             }
-            System.out.println("Calculo da linha "+i+": "+cal);
+            System.out.println("Calculo da linha " + i + ": " + cal);
         }
         System.out.println("\n");
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                for(int k = 0; k < n; k++){
-                    for(int l = 0; l < n; l++){
-                        if(num[i][j] == num[k][l] && i != k && j != l){
-                            iden += 1;
-                            repNum = num[i][j];
-                            System.out.println("O numero repetido e: "+num[i][j]+" repetindo: "+iden);
+        
+        /*for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = i; k < n; k++) {
+                    for (int l = j + 1; l < n; l++) {
+                        if (num[i][j] == num[k][l]) {
+                            int aux = auxNum;
+                            auxNum = num[i][j];
+                            if(auxNum != aux){
+                                repNum = 0;
+                            }
+                            else{
+                                repNum++;
+                            }
                         }
                     }
                 }
             }
+            if(repNum != 0){
+                System.out.println("O numero repetido e: " + auxNum + " repetindo: " + (repNum+1) + " vezes.");
+            }
+        }*/
+        
+        int somatorio = 0;
+        for(int i = 0; i < n; i++){
+            somatorio += num[i][i];
         }
+        System.out.println("Somatorio da diagonal principal: "+somatorio);
+        somatorio = 0;
+        for(int i = 0; i < n; i++){
+            somatorio += num[i][n - (i+1)];
+        }
+        System.out.println("Somatorio da diagonal segundario: "+somatorio);
     }
 }
